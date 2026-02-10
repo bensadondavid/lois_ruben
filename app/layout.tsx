137994b76a2928cd2feddb731fcac8ee6e425157
David <bensadondavidn@gmai.com>
2026-02-10 22:53:08 +0200

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,34 @@
 import "./globals.css";
+import { Lora, Pinyon_Script,  } from 'next/font/google'
+import localFont from "next/font/local";
 
+
+export const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-lora",
+  display: "swap",
+});
+
+export const pinyon = Pinyon_Script({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-pinyon",
+  display: "swap",
+});
+
+export const afrah = localFont({
+  src: "./fonts/afrah.otf",
+  variable: "--font-afrah",
+  display: "swap",
+});
+
+
+export const symphony = localFont({
+  src: "./fonts/symphony.woff2",
+  variable: "--font-symphony",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -7,8 +36,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr">
-      <body>
+    <html lang="fr" className={`${lora.variable} ${pinyon.variable} ${afrah.variable} ${symphony.variable}`} >
+      <body className="h-screen w-screen">
         <main>
           {children}
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
 
 function Home() {
 
   return (
 
-    <div>Home</div>
+    <div className='w-screen h-screen flex flex-col items-center justify-between bg-[url("/Backgrounds/lois-bg-accueil.webp")] bg-cover bg-center' >
+      <div className='flex flex-col justify-center items-center gap-2 mt-6'>
+        <Image
+          src="/Logo/logo-lois-entry.webp"
+          alt='logo-entry'
+          height={120}
+          width={120}
+          />
+        <p className='font-lora text-text-entry'>10.05.2026</p>
+      </div>
+      <div className='flex flex-col justify-center items-center mb-25'>
+        <Link href='/carte' className='w-fit font-afrah text-text-entry text-xl'>Voir la carte</Link>
+        <Image 
+            src='/Images/arc-link.png'
+            alt='arc-link'
+            height={30}
+            width={200}
+        />
+      </div>
+    </div>
     
   )
 
